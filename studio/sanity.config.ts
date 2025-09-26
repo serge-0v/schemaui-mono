88457051d2c4f8afd3ae3b60478e1c3d50b7317c
serge_0v <serge@schemaui.com>
2025-09-26 16:58:10 -0400

    fix: remove use client

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,9 +1,3 @@
-"use client";
-
-/**
- * This configuration is used to for the Sanity Studio that's mounted on the `/app/studio/[[...tool]]/page.tsx` route
- */
-
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
